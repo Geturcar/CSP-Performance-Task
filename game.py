7c1286c54f6d0edddf442226f3620e9d10531449
SteelRay013 <aneesh.anil013@gmail.com>
2023-03-22 11:42:08 -0400

    background

--- game.py
+++ game.py
@@ -1,9 +1,13 @@
 import pygame
-pygame.init()
-screen = pygame.display.set_mode((0,0),pygame.RESIZABLE)
-clock = pygame.time.Clock()
-running = True
 
+WIDTH = 1200
+HEIGHT = 600
+
+pygame.init()
+screen = pygame.display.set_mode((WIDTH,HEIGHT), pygame.RESIZABLE)
+clock = pygame.time.Clock()
+
+running = True
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
@@ -11,11 +15,11 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("green")
+    #INSIDE OF THE GAME LOOP
+    screen.blit(pygame.transform.scale(pygame.image.load("background.jpg"), (WIDTH, HEIGHT)), (0, 0))
 
     # RENDER YOUR GAME HERE
 
     # flip() the display to put your work on screen
     pygame.display.flip()
     clock.tick(60)  # limits FPS to 60
-pygame.quit()
